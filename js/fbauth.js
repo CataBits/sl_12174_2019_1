5f14683fdf1c072d67abdb4ee6ead6a248e8231a
Luferat <33375030+Luferat@users.noreply.github.com>
2020-03-03 11:42:44 -0300

    Autenticação Ok

--- js/fbauth.js
+++ js/fbauth.js
@@ -16,6 +16,12 @@ function authApp() {
     // Observador de usuários
     firebase.auth().onAuthStateChanged(userStatus);
 
+    // Monitora cliques no login
+    $(document).on('click', '.login', Login);
+
+    // Monitora cliques no logout
+    $(document).on('click', '.logout', Logout);
+
 }
 
 // Altera o status do usuário
@@ -24,25 +30,12 @@ function userStatus(userData) {
     if (userData) {
 
         // Fazer isso quando alguém está logado
-
-        // Atribuir dados ao usuário
-        user = userData;
-
-        _(user);
+        isLoged(userData);
 
     } else {
 
         // Fazer isso quando não tem usuário logado
-
-        // Mostra opção de login
-        var out = `
-<i class="fas fa-user-circle fa-fw"></i>
-<span>&nbsp;Logue-se...</span>
-<a href="#user" class="login"><i class="fas fa-fw fa-sign-in-alt"></i></a>
-        `;
-
-        // Atualiza o DOM
-        $('#usermenu').html(out);
+        notLoged();
 
     }
 }
@@ -61,10 +54,65 @@ function Login() {
 // Faz logout do usuário
 function Logout() {
 
-    // Faz logout
-    firebase.auth().signOut();
+    if (confirm('Tem certeza que deseja sair?')) {
 
-    // (opcional) Oculta o menu principal
-    //hideMenu();
+        // Faz logout
+        firebase.auth().signOut();
+
+        // (opcional) Oculta o menu principal
+        //hideMenu();
+
+    }
+
+}
+
+function isLoged(userData) {
+
+    // Atribuir dados ao usuário
+    user = userData;
+
+    // Limita o nome do usuário
+    var displayName = user.displayName.substr(0, 12);
+
+    // Mostra a opção de logout
+    var out = `
+<img src="${user.photoURL}" alt="${user.displayName}">
+<span>&nbsp;${displayName}</span>        
+<a href="#user" class="logout"><i class="fas fa-fw fa-sign-out-alt"></i></a>
+        `;
+
+    // Atualiza o DOM
+    $('#usermenu').html(out);
+
+    // Mostrar botão perfil no menu principal
+    $('#perfil').css('display', 'block');
+
+}
+
+function notLoged() {
+
+    // Mostra opção de login
+    var out = `
+<i class="fas fa-user-circle fa-fw"></i>
+<span>&nbsp;Logue-se...</span>
+<a href="#user" class="login"><i class="fas fa-fw fa-sign-in-alt"></i></a>
+        `;
+
+    // Atualiza o DOM
+    $('#usermenu').html(out);
+
+    // Ocultar botão perfil no menu principal
+    $('#perfil').css('display', 'none');
+
+    // Carrega o documento
+    $.get('pages/home.html', '', function(data) {
+
+        // Grava o documento na tag <main>
+        $('#main').html(data);
+
+        // Oculta o menu
+        // hideMenu();
+
+    });
 
 }
